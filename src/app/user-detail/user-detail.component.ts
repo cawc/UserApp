b2ea17760fd4d9a58bcbb4efe2625410e0dba07e
Robin <robinlievens24@gmail.com>
2019-05-17 10:37:25 +0200

    removed pointless import

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from '../user';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../user.service';
